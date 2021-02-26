8cfc7aecbcf9cfb8487c430dadd4dbfbf7c39dcd
Felipe Gonzalez <felipexgonzalez@gmail.com>
2021-02-26 16:49:56 -0600

    Agregar código de calibración

--- validacion/ejemplo/R/resumenes.R
+++ validacion/ejemplo/R/resumenes.R
@@ -9,3 +9,37 @@ histogramas_estaturas <- function(sims, sim_data, k = 10){
   ggplot(draws_df, aes(x = estatura, fill = tipo)) +
     geom_histogram() + facet_wrap(~.draw)
 }
+
+ajustar_diagnosticos <- function(rep, modelo, datos, params,
+                                 iter_sampling=2000, iter_warmup = 2000){
+
+  ajuste <- modelo$sample(data = datos,
+                          seed = 2210,
+                          iter_sampling = iter_sampling, iter_warmup = iter_warmup,
+                          refresh = 0,
+                          show_messages = FALSE)
+
+
+  suppressMessages(diagnostico <- ajuste$cmdstan_diagnose())
+  suppressMessages(resumen <- ajuste$summary())
+
+  # diagnosticar parámetros
+  sims_tbl <- ajuste$draws(names(params)) %>% as_draws_df() %>% as_tibble()
+  sbc_tbl <- sbc_rank(params, sims_tbl)
+  tibble(rep = rep, params = list(params), sbc_rank = list(sbc_tbl),
+         resumen = list(resumen), diagnosticos = list(diagnostico))
+}
+
+sbc_rank <- function(params_tbl, sims_tbl){
+  params_nom <- names(params_tbl)
+  sims_tbl_larga <- sims_tbl %>%
+    filter((row_number() %% 10) == 0) %>% # adelgazar la cadena
+    pivot_longer(cols = any_of(params_nom), names_to = "parametro", values_to = "valor")
+  params_tbl_larga <- params_tbl %>%
+    pivot_longer(cols = any_of(params_nom), names_to = "parametro", values_to = "valor_real")
+  sbc_tbl <- sims_tbl_larga %>%
+    left_join(params_tbl_larga, by = "parametro") %>%
+    group_by(parametro) %>%
+    summarise(sbc_rank = mean(valor_real < valor))
+  sbc_tbl %>% pivot_wider( names_from = "parametro", values_from ="sbc_rank")
+}
