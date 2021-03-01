ac26ca4dd00b4e81c7dde427be27d1d5154fdf8d
Felipe Gonzalez <felipexgonzalez@gmail.com>
2021-02-28 18:05:57 -0600

    Agregar ajustes a datos simulados y diagnósticos

--- validacion/modelo-base/R/funciones_calibracion.R
+++ validacion/modelo-base/R/funciones_calibracion.R
@@ -1,4 +1,4 @@
-obtener_muestra <- function(num_draw, frac= 0.04, seed = 1){
+obtener_muestra <- function(num_draw, frac= 0.04, seed = NA){
   sims_1 <- sims_casillas %>% filter(.draw == num_draw)
   muestra_1 <- quickcountmx::select_sample_prop(sims_1, stratum = estrato_df, frac = frac,
                                                 seed = seed)
@@ -7,16 +7,18 @@ obtener_muestra <- function(num_draw, frac= 0.04, seed = 1){
 }
 
 ajustar_diagnosticos <- function(rep, frac = 0.04, modelo, datos, params,
-                                 adapt_delta = 0.9,
+                                 adapt_delta = 0.9, max_treedepth = 10,
                                  iter_sampling=2000, iter_warmup = 2000){
 
-  datos_muestra <- obtener_muestra(rep, frac = frac, seed = 23)
+  datos_muestra <- obtener_muestra(rep, frac = frac, seed = NA)
   datos_1 <- c(datos, datos_muestra)
+  print(rep)
   ajuste <- modelo$sample(data = datos_1,
                           seed = 2210,
                           iter_sampling = iter_sampling, iter_warmup = iter_warmup,
                           parallel_chains = 4,
                           adapt_delta = adapt_delta,
+                          max_treedepth = max_treedepth,
                           refresh = 0,
                           show_messages = FALSE)
 
