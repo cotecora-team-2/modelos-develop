727de835fadd712846d7d03ad1cb976ccee4f8db
Felipe Gonzalez <felipexgonzalez@gmail.com>
2021-03-13 14:38:10 -0600

    Bajar datos de pin antes de reportes

--- validacion/modelo-base/generar-reportes-Rmd.R
+++ validacion/modelo-base/generar-reportes-Rmd.R
@@ -1,12 +1,32 @@
 library(tidyverse)
 
+## Bajar tiempos de llegada
+if(FALSE){
+  library(pins)
+  library(dotenv)
+  dotenv_file <- '.env'
+  path <- './datos'
+  load_dot_env(dotenv_file)
+
+  board_register_github(name = "mygithub",
+                      repo = "cotecora-team-2/quickcountmx-data",
+                      branch = 'main', token=Sys.getenv("token"))
+
+  sims <- pin_get("tiempos_simulaciones", board = "mygithub")
+  readr::write_rds(sims, paste(path, "simulaciones_completo.rds", sep = "/"))
+  rm(sims)
+}
+
+## parámetros de reportes
 params <- tibble(estado = c("NAYARIT", "CHIHUAHUA", "COLIMA", "ZACATECAS"),
                  frac = c(0.12, 0.06, 0.15, 0.15),
                  adapt_delta = c(0.95, 0.95, 0.95, 0.95),
-                 simular = c(FALSE, FALSE, FALSE, FALSE))
+                 simular = TRUE)
 
 render_reporte <- function(estado, frac, adapt_delta, simular, rmd_file) {
   print(estado)
+  print(frac)
+  print(simular)
   rmd_name <- basename(rmd_file) %>%
     str_remove(".Rmd")
   html_name <- paste0(rmd_name,"-",estado, "-", frac, ".html")
@@ -14,7 +34,7 @@ render_reporte <- function(estado, frac, adapt_delta, simular, rmd_file) {
   rmarkdown::render(rmd_file,
                     output_file = html_name,
                     params = list(estado = estado, frac = frac,
-                                  adapt_delta = adapt_delta)
+                                  adapt_delta = adapt_delta, simular = simular)
   )
 }
 
